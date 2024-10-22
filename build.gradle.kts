8e7f48db81de2e7f407f33ddfbf1b5574a092c65
Pavel Shareika <pavel.shareika@azati.com>
2024-10-22 18:53:30 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.intellij") version "1.17.2"
+}
+
+group = "by.shareiko"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2023.2.5")
+    type.set("IC") // Target IDE Platform
+
+    plugins.set(listOf("com.intellij.java"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("242.*")
+    }
+}
